c6cf8d3acfb8f4f02e7d8af21fbfa8700bc94a56
Hollin R. Wilkins <hrw7@cornell.edu>
2011-04-09 03:35:04 +0300

    ignoring package directory, got rid of install.rb/uninstall.rb

--- pkg/acts_as_taggable_simple-0.0.1/lib/acts_as_taggable_simple.rb
+++
@@ -1,5 +0,0 @@
-# ActsAsTaggableSimple
-
-require 'tag'
-require 'tagging'
-require 'active_record/acts/taggable'
--- pkg/acts_as_taggable_simple-0.0.1/spec/models.rb
+++
@@ -1,6 +0,0 @@
-class TaggableModel < ActiveRecord::Base
-  acts_as_taggable
-end
-
-class UntaggableModel <  ActiveRecord::Base
-end
